5ad521c09137ca79ca96cb44ace895e99cb5fbaa
KoxSosen <whyconfused2@gmail.com>
2021-01-30 18:24:42 +0100

    add mysql to the bot

--- nodebot/discordbot.js
+++ nodebot/discordbot.js
@@ -1,19 +1,35 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
+var mysql = require('mysql');
 const client = new Discord.Client();
  
-// Set the prefix
+ 
 const prefix = ".";
 client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
 
-  if (message.content.startsWith(prefix + "ping")) {
+  if (message.content.startsWith(prefix + "bal")) {
     message.channel.send("pong!");
   } else
-  if (message.content.startsWith(prefix + "foo")) {
+  if (message.content.startsWith(prefix + "nou")) {
     message.channel.send("bar!");
-  }
+  } else
+})  
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
+  });
 });
  
 client.login(config.token);
