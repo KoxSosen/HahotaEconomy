730dc179da52936fe32e9bd3d956b17a84c3fb69
KoxSosen <whyconfused2@gmail.com>
2021-01-31 19:47:08 +0100

    add db config support

--- nodebot/discordbot.js
+++ nodebot/discordbot.js
@@ -2,34 +2,27 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 var mysql = require('mysql');
 const client = new Discord.Client();
- 
- 
-const prefix = ".";
-client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
 
-  if (message.content.startsWith(prefix + "bal")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "nou")) {
-    message.channel.send("bar!");
-  }  
-});
-
+ 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + result);
-  });
+con.connect(err => {
+    // Console log if there is an error
+    if (err) return console.log(err);
+
+    // No error found?
+    console.log(`MySQL has been connected!`);
 });
+
+client.on('ready', () => {
+    // Log when bot is ready
+    console.log(`${client.user.tag} is online!`);
+});
+		
  
 client.login(config.token);
