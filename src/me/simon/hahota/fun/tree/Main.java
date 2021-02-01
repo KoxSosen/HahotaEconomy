701978f348cf9f897fa0018a30266b792c9f3f16
KoxSosen <whyconfused2@gmail.com>
2021-02-01 16:19:42 +0100

    add PlayerJoinEvent

--- src/me/simon/hahota/fun/tree/Main.java
+++ src/me/simon/hahota/fun/tree/Main.java
@@ -5,12 +5,16 @@ import java.sql.SQLException;
 
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.simon.hahota.fun.tree.sql.MySQL;
 import me.simon.hahota.fun.tree.sql.SQLGetter;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
  
     public MySQL SQL;
@@ -32,6 +36,8 @@ public class Main extends JavaPlugin {
     	
     if (SQL.isConnected()) {
     	Bukkit.getLogger().info("Suxessfully connected");
+    	data.createTable();
+    	this.getServer().getPluginManager().registerEvents(this, this);
     }
     	
     			
@@ -42,4 +48,11 @@ public class Main extends JavaPlugin {
 		SQL.disconnect();
     }
 
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		data.createPlayer(player);
+		
+	}
+	
 }
