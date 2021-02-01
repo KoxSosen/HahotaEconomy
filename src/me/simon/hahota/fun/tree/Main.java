9888b5fdbd0f932761497b06346ac535510e84d6
KoxSosen <whyconfused2@gmail.com>
2021-02-01 16:14:59 +0100

    main class

--- src/me/simon/hahota/fun/tree/Main.java
+++ src/me/simon/hahota/fun/tree/Main.java
@@ -3,21 +3,25 @@ package me.simon.hahota.fun.tree;
 
 import java.sql.SQLException;
 
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.simon.hahota.fun.tree.sql.MySQL;
+import me.simon.hahota.fun.tree.sql.SQLGetter;
 
 public class Main extends JavaPlugin {
 
  
     public MySQL SQL;
+    public SQLGetter data;
 
 	@Override
     public void onEnable() 	{
 	this.getConfig().options().copyDefaults();
 	saveDefaultConfig();
     	this.SQL = new MySQL();
+        this.data = new SQLGetter(this);
     	
     	try  { 	
     		SQL.connect();
